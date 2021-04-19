009133f549b1c8debb99740c24252cc6c8444632
Benjamin Robertson <ben.robertson@puppet.com>
2021-04-19 16:20:25 +1000

    Site commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,5 +4,41 @@
 #
 # @example
 #   include dog_site
-class dog_site {
+class dog_site (
+  String $favourite = 'Poodles',
+)
+{
+
+  file {'c:\inetpub\dogsite':
+    ensure => directory,
+    owner  => system,
+  }
+
+  file {'c:\inetpub\dogsite\dogs.html':
+    ensure  => file,
+    content => epp('dogs.epp', {'favourite' => $favourite, 'servertype' => $facts['os']['windows']['product_name']}),
+  }
+
+  acl { 'c:\inetpub\dogsite':
+    permissions => [
+      { identity => 'IIS_IUSRS', rights => ['read','execute'] },
+    ],
+  }
+
+  # Configure Cat site
+  iis_site { 'dogs':
+    ensure           => 'started',
+    physicalpath     => 'c:\inetpub\dogsite',
+    applicationpool  => 'DefaultAppPool',
+    enabledprotocols => 'http',
+    bindings         => [
+      {
+        'bindinginformation' => '*:80:dogs',
+        'protocol'           => 'http',
+      }
+    ],
+    defaultpage      => 'dogs.html',
+    name             => 'dog web site',
+  }
+
 }
